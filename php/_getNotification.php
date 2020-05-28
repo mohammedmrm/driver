cf5de5572b02ae68c4ebf6e0daa47ce6f2c0a841
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-05-28 15:20:07 +0300

    offline mode

--- php/_getNotification.php
+++ php/_getNotification.php
@@ -15,7 +15,7 @@ $user_id = $_SESSION['userid'];
 $sql = 'select count(*) as unseen from notification where for_client = 0 and staff_id = ? and driver_seen=0';
 $res = getData($con,$sql,[$user_id]);
 $unseen = $res[0]['unseen'];
-$sql = 'select * from notification where for_client = 0 and staff_id = ? order by date DESC limit 20';
+$sql = 'select * from notification where for_client = 0 and staff_id = ? order by date DESC limit 50';
 $result = getData($con,$sql,[$user_id]);
 $success = 1;
 echo json_encode(['success'=>$success,"data"=>$result,'unseen'=>$unseen]);
--- top-menu.php
+++ top-menu.php
@@ -4,5 +4,5 @@
 <div class="header header-fixed header-logo-center">
         <a href="index.php" class="header-title"><?php echo $config['Company_name'];?></a>
 		<a href="#" class="back-button header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
-		<a href="#" data-toggle-theme-switch class="header-icon header-icon-4">خروج</a>
+		<a href="logout.php" data-toggle-theme-switch class="header-icon header-icon-4">خروج</a>
 </div>
