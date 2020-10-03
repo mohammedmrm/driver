8fa2ccfca7984ff1f9eeb9149edb77d838282df2
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-03 21:53:52 +0300

    api sync update

--- php/_orderChange.php
+++ php/_orderChange.php
@@ -39,7 +39,7 @@ if($v->passes()) {
     $success = 1;
     $sql = 'insert into tracking (order_status_id,note,order_id,new_address,staff_id) values(?,?,?,?,?)';
     $result = setData($con,$sql,['8','تغير العنوان',$order_id,$address,$id]);
-    $sql = "select staff.token as s_token, clients.token as c_token from orders inner join staff
+    $sql = "select staff.token as s_token, orders.id as id , clients.sync_dns as dns, clients.sync_token as token, orders.isfrom as isfrom, clients.token as c_token from orders inner join staff
             on
             staff.id = orders.manager_id
             or
@@ -47,6 +47,17 @@ if($v->passes()) {
             inner join clients on clients.id = orders.client_id
             where orders.id =  ?";
     $res =getData($con,$sql,[$order_id]);
+    if($res[0]['isfrom'] == 2){
+       $response = httpPost($res[0]['dns'].'/api/orderStatusSync.php',
+        [
+         'token'=>$res[0]['token'],
+         'status'=>8,
+         'note'=>'تغير العنوان - '.$note,
+         'city'=>$city,
+         'town'=>$town,
+         'id'=>$order_id,
+        ]);
+    }
     $sql = "update orders set driver_id=?,to_city=?,to_town=? where id=?";
     setData($con,$sql,[$driver[0]['driver_id'],$city,$town,$order_id]);
     sendNotification([$res[0]['s_token'],$res[1]['s_token'],$res[0]['c_token']],[$order_id],'طلب رقم  ',"تغير عنوان الطلب - ".$note,"../orderDetails.php?o=".$order_id);
