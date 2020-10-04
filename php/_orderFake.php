df40dde82526aeb0583ea2fed25fc670a986259d
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-04 17:51:26 +0300

    bugs fix

--- php/_orderFake.php
+++ php/_orderFake.php
@@ -26,7 +26,17 @@ $v->validate([
     'note'      => [$note,  'required|max(250)'],
     'order_id'  => [$order_id,'required|int']
 ]);
-
+function httpPost($url, $data)
+{
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
 if($v->passes()) {
 
    $sql = 'update orders set order_status_id =?,new_price=?
@@ -68,5 +78,5 @@ if($v->passes()) {
            'order_id'=>implode($v->errors()->get('order_id'))
            ];
 }
-echo json_encode([$response,'success'=>$success, 'error'=>$error,$_POST]);
+echo json_encode([json_decode(substr($response, 3)),'success'=>$success, 'error'=>$error,$_POST]);
 ?>
