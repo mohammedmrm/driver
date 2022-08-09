2c4fd3088f166191bdcde7175b50904ae77ceb1b
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-08-10 00:43:41 +0200

    api update

--- api/config.php
+++ api/config.php
@@ -1,34 +1,34 @@
 <?php
-ob_start(); 
+ob_start();
 error_reporting(0);
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require_once("../php/dbconnection.php");
 require_once("_apiAccess.php");
 access();
-try{
-  $msg="";
+try {
+  $msg = "";
   $config = [
-     "Company_name"=>"شركة النهر",
-     "Company_phone"=>"0782222222",
-     "wellcome_message"=>"اعلان",
-     "Company_email"=>"nahar@nahar.com",
-     "Company_logo"=>"img/logos/logo.png",
-     "dev_b"=>5000,                 //??? ??????? ?????
-     "dev_o"=>10000,                //??? ??????? ????? ?????????
-     "weightPrice"=>1000,           //??? ??????? ????? ?????????
+    "Company_name" => "شركة النهر",
+    "Company_phone" => "0782222222",
+    "wellcome_message" => "اعلان",
+    "Company_email" => "nahar@nahar.com",
+    "Company_logo" => "img/logos/logo.png",
+    "dev_b" => 5000,                 //??? ??????? ?????
+    "dev_o" => 10000,                //??? ??????? ????? ?????????
+    "weightPrice" => 1000,           //??? ??????? ????? ?????????
   ];
   $sql = "select * from setting";
-  $setting = getData($con,$sql);
-  foreach($setting as $val){
+  $setting = getData($con, $sql);
+  foreach ($setting as $val) {
     $config[$val['control']] =  $val['value'];
   }
-}catch(PDOException $ex) {
-   $data=["error"=>$ex];
-   $success="0";
-   $msg = "Query Error";
+} catch (PDOException $ex) {
+  $data = ["error" => $ex];
+  $success = "0";
+  $msg = "Query Error";
 }
-$config['d_ad1'] = strip_tags($config['d_ad1']);
-$config['d_ad2'] = strip_tags($config['d_ad2']);
+$config['d_ad1'] = strip_tags(trim($config['d_ad1']));
+$config['d_ad2'] = strip_tags(trim($config['d_ad2']));
 ob_end_clean();
-echo json_encode(['code'=>200,'message'=>$msg,"success"=>$success,'config'=>$config]);
+echo json_encode(['code' => 200, 'message' => $msg, "success" => $success, 'config' => $config]);
