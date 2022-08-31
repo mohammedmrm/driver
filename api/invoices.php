ba1a6ff94f277c8a8747b1389214352f2b1efc0e
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-08-31 21:54:51 +0200

    bug fix

--- api/invoices.php
+++ api/invoices.php
@@ -72,6 +72,7 @@ try {
           sum(discount) as discount,
           count(orders.id) as orders
           from orders
+          left join towns on towns.id = orders.to_town
           left JOIN client_dev_price on client_dev_price.client_id = orders.client_id AND client_dev_price.city_id = orders.to_city
           where orders.driver_id = ?  and driver_invoice_id = 0 and (order_status_id = 4 or order_status_id = 5 or order_status_id = 6)  and orders.confirm=1
           ";
