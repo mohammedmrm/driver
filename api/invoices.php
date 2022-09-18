a0f5a30208e0399c3a4b6309a71c906cda4bd4f3
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-09-18 16:20:00 +0200

    try

--- api/invoices.php
+++ api/invoices.php
@@ -47,9 +47,18 @@ try {
 
   $data = getData($con, $sql2, [$userid]);
 
+  $sqlDP = "SELECT driver_price/sum(if(order_status_id = 4 or order_status_id = 4 or order_status_id = 6,1,0)) as price
+  FROM `driver_invoice` 
+  INNER JOIN orders on driver_invoice.id = orders.driver_invoice_id 
+  WHERE driver_price > 0 and driver_invoice.driver_id = ? 
+  GROUP by driver_invoice.id
+  ORDER by driver_invoice.date DESC 
+  limit 1";
+  $dp = getData($con, $sql, [$$userid]);
+  $dp[0]['price'] ? '' : $dp[0]['price'] = $config['driver_price'];
+
   $sql = "select
           sum(new_price) as income,
-
           sum(
               new_price -
               (if(to_city = 1,
@@ -70,6 +79,7 @@ try {
                  + if(weight > 1 ,( (weight-1) * " . $config['weightPrice'] . " ),0)
              )) as client_price,
           sum(discount) as discount,
+          SUM(IF (order_status_id = '4' or order_status_id = '5' or order_status_id = '6',1,0)) as  recived,
           count(orders.id) as orders
           from orders
           left join towns on towns.id = orders.to_town
@@ -93,5 +103,6 @@ try {
 }
 $total['start'] = date('Y-m-d', strtotime($start));
 $total['end'] = date('Y-m-d', strtotime($end . " -1 day"));
+$total['orders'] = $total['orders'] . " ( " . $total['recived'] * $db[0]['price'] . " ) ";
 ob_end_clean();
 echo json_encode([$userid, $sql, 'code' => $code, 'message' => $msg, 'success' => $success, 'data' => $data, "total" => $total]);
