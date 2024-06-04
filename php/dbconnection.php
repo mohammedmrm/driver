c51f07d693b4ae2483d4b1cd4c74734e7faef37c
Mohammed Ridha M. M <60153318+mohammedmrm@users.noreply.github.com>
2024-06-04 12:15:38 +0200

    Update dbconnection.php

--- php/dbconnection.php
+++ php/dbconnection.php
@@ -1,4 +1,5 @@
 ﻿<?php
+require_once("../api/_apiAcccess.php")
 try{
 
 $con = new PDO('mysql:host=localhost;dbname=nahar', "root",
@@ -16,7 +17,7 @@ function getData($db,$query,$parm = []) {
 }
 function setData($db, $query, $parm = [])
 {
-  $userid = $_SESSION['userid'] ? $_SESSION['userid'] : -1;
+  $userid = $_SESSION['userid'] ? $_SESSION['userid'] : $userid ? $userid : -1;
   $stmt = $db->prepare($query);
   $stmt->execute($parm);
   $count = $stmt->rowCount();
